fb5b79e5c6e63c341285ed9a7c9cc88b6e44a977
Zac Hmich <zac@MacBook-Pro-de-Zac.local>
2018-02-24 09:58:27 +0100

    inté home suite

--- wp-content/themes/sanishop/templates/home.php
+++ wp-content/themes/sanishop/templates/home.php
@@ -284,32 +284,55 @@
             <footer>
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <div class="logo-footer">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/src/images/sanishop_noir.jpg" alt="">
                                 <div class="footer-icons">
-                                    <i class="fab fa-facebook-f" style="font-size:2em;color:#fff"></i>
-                                    <i class="fab fa-twitter" style="font-size:2em;color:#fff"></i>
-                                    <i class="fab fa-google-plus-g" style="font-size:2em;color:#fff"></i>
-                                    <i class="fas fa-phone" style="font-size:2em;color:#fff"></i>
-                                    <i class="fas fa-envelope" style="font-size:2em;color:#fff"></i>
+                                    <i class="fab fa-facebook-f" style="font-size:1em;color:#fff"></i>
+                                    <i class="fab fa-twitter" style="font-size:1em;color:#fff"></i>
+                                    <i class="fab fa-google-plus-g" style="font-size:1em;color:#fff"></i>
+                                    <i class="fas fa-phone" style="font-size:1em;color:#fff"></i>
+                                    <i class="fas fa-envelope" style="font-size:1em;color:#fff"></i>
                                 </div>
                             </div>
+                        </div>
+                        <div class="col-md-3 mission">
+                            <h3 class="footer-title">
+                                Notre mission
+                            </h3>
+                            <p class="paragraph">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consectetur est in, iste nisi ratione suscipit velit voluptatum! Beatae blanditiis corporis cumque expedita maxime, modi praesentium sequi sunt vero. Consectetur?
+                            </p>
                         </div>
                         <div class="col-md-3 wrapper-footer">
                             <h3 class="footer-title">
                                 plan du site
                             </h3>
                             <ul class="list-footer">
-                                <li class="list-footer list-footer--item">item</li>
-                                <li class="list-footer list-footer--item">item</li>
-                                <li class="list-footer list-footer--item">item</li>
-                                <li class="list-footer list-footer--item">item</li>
-                                <li class="list-footer list-footer--item">item</li>
-                                <li class="list-footer list-footer--item">item</li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
                             </ul>
                         </div>
-                        <div class="col-md-3 wrapper-footer"></div>
+                        <div class="col-md-3 wrapper-footer">
+                            <h3 class="footer-title">
+                                Partenaires
+                            </h3>
+                            <ul class="list-footer">
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                                <li class="list-footer--item"><a href="#">Item</a></li>
+                            </ul>
+                            <h3 class="footer-title">
+                                Aide
+                            </h3>
+                            <h3 class="footer-title">
+                                Contactez-nous
+                            </h3>
+                        </div>
                     </div>
                 </div>
             </footer>
